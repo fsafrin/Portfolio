badf807b70d7e3c81021747761f57bcccb65c311
Sayeda Farhan-E-Safrin <107347519+fsafrin@users.noreply.github.com>
2024-12-02 21:45:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function () {
+    // Select the div container
+    var animationContainer = document.getElementById('lottie-animation');
+
+    // Specify the path to your JSON file
+    var animationData = {
+        container: animationContainer, // the dom element that will contain the animation
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './assets/Animation - 1733152474263.json' // the path to the animation json
+    };
+
+    // Load the animation
+    var anim = bodymovin.loadAnimation(animationData);
+});
+
